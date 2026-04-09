a15cd3928e6749b247f65dbdec305b1a81ec4a54
Anthony <anthonyblur13@gmail.com>
2026-04-08 23:46:47 -0400

    initial commit

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const pool = require('./db')
+const authRoutes = require('./routes/auth.js')
+
+
+const app = express()
+
+dotenv.config()
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/auth', authRoutes)
+
+app.get('/api/test', (req, res) => {
+    res.json({ message: 'Server is running' })
+})
+
+pool.query('SELECT NOW()', (err, res) => {
+    if (err) {
+        console.log('Database connection failed', err)
+    } else {
+        console.log('Database connected:', res.rows[0].now)
+    }
+})
+
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
---
+++ server/routes/auth.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const router = express.Router()
+const pool = require('../db')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+
+
+
+router.post('/register', async (req, res) => {
+    try {
+        const { name, email, password } = req.body
+        if (!name || !password || !email) {
+            return res.status(400).json({ message: 'Please fill in all required fields' })
+        }
+        const result = await pool.query('SELECT * FROM users WHERE email = $1', [email])
+
+        if (result.rows.length > 0) {
+            return res.status(400).json({ message: 'Email is already registered' })
+        }
+
+        const hash = await bcrypt.hash(password, 10)
+
+        const newUser = await pool.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3)', [name, email, hash])
+        return res.status(201).json({ message: 'Account Created! :)' })
+    }
+
+    catch (err) {
+        return res.status(500).json({ message: 'Server error' });
+    }
+
+})
+
+
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body
+
+    }
+
+    catch (err) {
+        return res.status(500).json({ message: 'Server error' });
+    }
+}
+
+)
+
+module.exports = router
+
+
